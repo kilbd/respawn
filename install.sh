7ae7e13d5a94f0bfa6517efa1cd1008c7161625d
Drew Kilb <github@git.kilb.email>
2025-11-15 21:33:33 -0500

    feat: Improve install script to report failures

--- install.sh
+++ install.sh
@@ -11,18 +11,21 @@ set +e
 if ! git ls-remote -q "$HOME/respawn" > /dev/null 2>&1; then
   git clone https://github.com/kilbd/respawn.git "$HOME/respawn"
 fi
-set -e
 
 cd respawn
+failed=()
 while IFS= read -r -d '' script; do
     if [[ -x "$script" ]]; then
         source "$script"
+        rc=$?
+        if [[ $rc -ne 0 ]]; then
+            failed+=("$script:$rc")
+        fi
     fi
 done < <(find "./ubuntu" -name "*.sh" -print0)
 cd -
 
 # Clone full dotfiles repo if missing
-set +e
 if ! git ls-remote -q "$HOME/dotfiles" > /dev/null 2>&1; then
   git clone https://github.com/kilbd/dotfiles.git "$HOME/dotfiles"
 fi
@@ -30,3 +33,13 @@ set -e
 
 echo 'Linking dotfiles...'
 source "$HOME/dotfiles/setup/link.sh"
+
+if [[ ${#failed[@]} -gt 0 ]]; then
+    echo
+    echo "SUMMARY: Some scripts failed:"
+    for script in "${failed[@]}"; do
+        echo " - $script"
+    done
+    echo
+    echo "You can try to fix them and run again manually."
+fi
