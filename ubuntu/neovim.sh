c96f6ead4aadb2c888f941e535700397e4d386c3
Drew Kilb <github@git.kilb.email>
2025-12-12 13:54:44 -0500

    fix: Use correct Neovim file name

--- ubuntu/neovim.sh
+++ ubuntu/neovim.sh
@@ -3,10 +3,10 @@
 set -e
 
 cd /tmp
-wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz"
 tar -xvzf nvim.tar.gz
-sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-sudo cp -R nvim-linux64/lib /usr/local/
-sudo cp -R nvim-linux64/share /usr/local/
-rm -rf nvim-linux64 nvim.tar.gz
+sudo install nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux-x86_64/lib /usr/local/
+sudo cp -R nvim-linux-x86_64/share /usr/local/
+rm -rf nvim-linux-x86_64 nvim.tar.gz
 cd -
